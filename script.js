c9e5b766496314572b385f4cf38d9db6d822dc3b
pyatasenyuk <36247802+pyatasenyuk@users.noreply.github.com>
2018-02-23 21:21:19 +0300

    T

---
+++ script.js
@@ -0,0 +1,57 @@
+
+
+var forths = document.games.gen.value
+var fifth = document.games.exercise.value= GetRandomInt
+
+function GetRandomInt(min,max) {
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+firstses=min
+secondes=max
+}
+/*
+
+{
+    document.games.gen.onclick
+
+    var m =  2; s = '', r = "dfgd";
+    for (var i=0; i < m; i++) { s += r.charAt(Math.floor(Math.random()*r.length)); }
+    return s;
+*/
+
+var firstses = document.games.min.value
+var secondes = document.games.max.value
+    document.games.gen.onclick = function() {
+    console.log(GetRandomInt)
+}
+*/
+
+
+
+/*
+function validateForm() {
+
+
+    var firstname = document.contact.names.value;
+
+    var secondname = document.contact.Reversed.value;
+
+
+    if (secondname === firstname.split('').reverse().join('')) {
+        alert("Good job!")
+        return true;
+
+
+    }
+    else {
+        alert("Reverse that!")
+        return false;
+
+    }
+}
+*/
+
+
+
+
+
